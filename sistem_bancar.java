bf448f946e2fadae41e08a121d647439addc4de2
Darius Spiridon <dariusspiridon@MacBook-Air-Darius.local>
2026-09-08 14:49:47 +0300

    Adaugare metoda initiazaTransferBancar

--- sistem_bancar.java
+++ sistem_bancar.java
@@ -3,10 +3,17 @@ import java.util. *;
 class SoldNegativ extends Exception{};
 class FonduriInsuficienteException extends Exception{};
 class PinInvalidException extends Exception{};
-class CardBlocatExceptipon extends Exception{};
+class CardBlocatException extends Exception{};
 class ListaConturiGoalaException extends Exception{};
 class FormatNrTelefonInvalid extends Exception{};
 class IbanDuplicat extends Exception{};
+class IbanInvalid extends Exception{};
+class ContInexistent extends Exception{};
+class FormatIbanInvalid extends Exception{};
+class IbanEmitorInvalid extends Exception{};
+class IbanAcceptorInvalid extends Exception{};
+class SoldInsuficient extends Exception{};
+class SumaNegativaException extends Exception{};
 
 
 class ContBancar{
@@ -58,8 +65,9 @@ abstract class Client{
     private String adresa;
     private String nrTelefon;
     private HashMap <String, ContBancar> conturi;
+    private Banca banca;
 
-    public Client(String nume, String adresa, String nrTelefon) throws FormatNrTelefonInvalid{
+    public Client(String nume, String adresa, String nrTelefon, Banca banca) throws FormatNrTelefonInvalid{
         this.nume = nume;
         this.adresa = adresa;
         if(nrTelefon != null &&(nrTelefon.length() != 10 || nrTelefon.substring(0, 1).equals("0") == false)){
@@ -67,16 +75,27 @@ abstract class Client{
         }
         this.nrTelefon = nrTelefon;
         this.conturi = new HashMap<>();
+        this.banca = banca;
     }
 
-    public boolean creeazaContBancar(String iban, double sold, int pin) throws IbanDuplicat{
-        if(verificaDuplicatIban(iban) == true){
-            throw new IbanDuplicat();
-            return false;
+    public ContBancar creeazaContBancar(String iban, double sold, int pin) throws SoldNegativ, FormatIbanInvalid, IbanDuplicat{
+        if(sold < 0.00) throw new SoldNegativ();
+
+        else if(iban == null || iban.length() != 24){
+            throw new FormatIbanInvalid();
         }
+
+        else if(iban.substring(0, 2) != "RO" || iban.substring(4,       8).equals(this.banca.getCodBanca())){
+            throw new FormatIbanInvalid();
+        }
+
+        else if(this.conturi.containsKey(iban)){
+            throw new IbanDuplicat();
+        }
+
         ContBancar contNou = new ContBancar(iban, this.nume, sold, pin);
-        conturi.put(contNou.getIban(), contNou);
-        return true;
+        this.conturi.put(iban, contNou);
+        return contNou;
     }
 
     public boolean stergeContBancar(String iban){
@@ -85,5 +104,35 @@ abstract class Client{
             return true;
         }
         return false;
+    }
+
+    public void afiseazaInformatiiCont(String iban) throws ContInexistent{
+        ContBancar contCautat = this.conturi.get(iban);
+        if(contCautat != null){
+            System.out.println("Cont gasit:\n" + contCautat.toString());
+        }
+        else{
+            throw new ContInexistent();
+        }
+    }
+
+    public void initiazaTransferBancar(String ibanEmitor, String ibanAcceptor, double suma) throws IbanEmitorInvalid, IbanAcceptorInvalid, SoldInsuficient, SumaNegativaException{
+        if(this.conturi.containsKey(ibanEmitor) == false){
+            throw new IbanEmitorInvalid();
+        }
+
+        else if(ibanAcceptor == null || ibanAcceptor.startsWith("RO") == false || ibanAcceptor.length() != 24 || this.banca.coduriBanci.containsKey(ibanAcceptor.substring(4, 8)) == false){
+            throw new IbanAcceptorInvalid();
+        }
+
+        ContBancar contEmitor = this.conturi.get(ibanEmitor);
+        if(suma > contEmitor.getSold()){
+            throw new SoldInsuficient();
+        }
+
+        else if(suma < 0.0){
+            throw new SumaNegativaException();
+        }
+        this.banca.proceseazaTransfer(ibanEmitor, ibanAcceptor, suma);
     }
 }
